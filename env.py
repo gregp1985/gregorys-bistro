3ff68e4465df75888feecd82cbec6da2e0104768
gregp1985 <greg@gwphillips.co.uk>
2026-01-15 11:18:17 +0000

    Postgres Database setup

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_PhO5VqAXzvM7@ep-wispy-math-agodyf7v.c-2.eu-central-1.aws.neon.tech/point_spoke_stall_837700"
+)
