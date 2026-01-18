c300e9effaee250550bcc70c517d8b73632c9843
gregp1985 <greg@gwphillips.co.uk>
2026-01-18 17:16:06 +0000

    adjustments to view and booking html for view and cancel of bookings

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import BookingForm
 from .models import Booking
@@ -28,3 +28,28 @@ def make_booking(request):
         "my_bookings": my_bookings,
     }
     return render(request, "booking.html", context)
+
+
+def cancel_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id, user=request.user)
+    booking.status = 'CANCELLED'
+    booking.save()
+    return redirect('make_booking')
+
+
+def edit_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id, user=request.user)
+
+    if request.method == "POST":
+        form = BookingForm(request.POST, instance=booking, user=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("make_booking")
+    else:
+        form = BookingForm(instance=booking, user=request.user)
+
+    return render(
+        request,
+        "edit_booking.html",
+        {"form": form, "booking": booking}
+    )
