d51c9518f75cfecf8bd5ba44cc7c795ab574c62a
gregp1985 <greg@gwphillips.co.uk>
2026-01-18 12:40:30 +0000

    Move Secret key for authentication to env.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&e)lwizzv!#e*kshcau1)6c#wn=@b7fy&wjga!9jfw&3ez1f*e'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
