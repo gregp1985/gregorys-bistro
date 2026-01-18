5a661462028a51cda3521c1649ad002ed370b135
gregp1985 <greg@gwphillips.co.uk>
2026-01-18 13:58:48 +0000

    Setup of apps and initial home page

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from booking.views import book_table
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('booking/', book_table, name='booking')
+    path('booking/', book_table, name='booking'),
+    path('', include("home.urls"), name='home-urls'),
 ]
