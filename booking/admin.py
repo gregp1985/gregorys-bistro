e8736ac144b8900227bee576e22173f28e233ffa
gregp1985 <greg@gwphillips.co.uk>
2026-02-01 13:08:21 +0000

    Addition of admin filter for bookings

--- booking/admin.py
+++ booking/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 from .models import Booking, OpeningHours, Table
 
-admin.site.register(Booking)
+
 admin.site.register(OpeningHours)
 admin.site.register(Table)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "reference",
+        "start_time",
+        "name",
+        "party_size",
+        "status",
+    )
+
+    list_filter = (
+        "start_time",
+        "name",
+    )
+
+    search_fields = (
+        "reference",
+        "name__username",
+        "name__email",
+    )
+
+    date_hierarchy = "start_time"
